090f27f018cda1adb5f13f8e7f66af297712bd7f
madeyemartini <GHty1212!>
2022-04-07 00:00:44 +0100

    committing src folder

---
+++ src/main/java/com/example/leaseloco/LeaselocoApplication.java
@@ -0,0 +1,13 @@
+package com.example.leaseloco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeaselocoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeaselocoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/leaseloco/LeaselocoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.leaseloco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeaselocoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
